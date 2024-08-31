84d07db618688a43e18c89beb7d2276c6fca341a
ReapingHunter <manalilimarlex@gmail.com>
2024-08-31 19:04:55 +0800

    Reverted personalized greeting feature

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 
-const readline = require("readline");
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question("What is your name: ", (answer) => {
-    console.log(`Hello, ${answer}!`);
-    rl.close();
-})
+console.log("Hello World")
