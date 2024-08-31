26c7ab9e07d7b5d2e4d8f02b70938e3a5953b57e
ReapingHunter <manalilimarlex@gmail.com>
2024-08-31 19:28:02 +0800

    Resolved merge conflict in file

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 
-console.log("Hello World")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name: ", (answer) => {
+    console.log(`Hello, ${answer}!`);
+    rl.close();
+})
